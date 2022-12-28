4e7c1fd741bbc279e4f6c87d0027115d15562e28
planb-yano <takahiro.yano@plan-b.co.jp>
2022-12-29 00:45:26 +0900

    絶対パスの設定

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,18 @@
+const path = require("path");
+
 module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        "^@(.*)$": "<rootDir>/src$1",
+      },
+    },
+  },
   babel: {
     presets: [
       [
